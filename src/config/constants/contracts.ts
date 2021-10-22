92ee5cf6db686dd7ee4be3fe14a2671c3acd2662
lion cho <chowin21@gmail.com>
2021-10-22 20:58:26 +0900

    fix(testnet): Added chef

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,7 +1,7 @@
 export default {
   masterChef: {
-    97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
-    56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
+    97: '0xC21abe7BFFF4E6612898d2FdC41BC8AA6cb117f9',
+    56: '',
   },
   sousChef: {
     97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
